2d44f8dd05daf16b08c4c828658fd8208aa03e7f
cmccarthy123 <Guest@stk-cs01-ws03.westlothian.sch.uk>
2015-11-26 09:44:16 +0000

    sprint1.1

--- pupil.java
+++ pupil.java
@@ -1,1 +1,19 @@
 
+public class PUPIL
+{
+   private String firstName;
+   private String surname;
+   private int examMark; 
+   
+
+
+    public PUPIL()
+      {
+          //constructor
+          firstName "";
+          surname "";
+          examMark 0;
+          
+   }
+
+}
