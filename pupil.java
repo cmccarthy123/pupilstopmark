4268539c30bc566178e2034816b04e9ce243d3b0
Guest User <Guest@stk-cs01-ws03.westlothian.sch.uk>
2015-12-01 10:30:38 +0000

    1.5

--- pupil.java
+++ pupil.java
@@ -1,19 +1,42 @@
 
 public class PUPIL
 {
-   private String firstName;
-   private String surname;
-   private int examMark; 
-   
-
+    private String firstName;
+    private String surname;
+    private int examMark; 
 
     public PUPIL()
-      {
-          //constructor
-          firstName "";
-          surname "";
-          examMark 0;
-          
-   }
+    {
+        //constructor
+        firstName = "";
+        surname = "";
+        examMark = 0;
 
+    }
+
+    public void readPupilDetails(String dataItems)
+    {
+
+        // unpack string of row data into fields
+        String[] rowItems = dataItems.split(",");
+
+        // store each data item as instance property
+        firstName = rowItems[0];
+        surname = rowItems[1];
+        examMark = (Integer.parseInt(rowItems[2]));
+
+    }
+
+    public String writeDetails()
+    {
+        // join up data into a string to output as a row
+        // use "," to separate csv columns
+        String pupilData = "";
+        pupilData = pupilData.concat(firstName);
+        pupilData = pupilData.concat(",");
+        pupilData = pupilData.concat(surname);
+        pupilData = pupilData.concat(",");
+        pupilData = pupilData.concat(Integer.toString(examMark));
+        return pupilData;
+    }
 }
